d3df178cf9374d2f61a214709d0cbad83ba3c2e7
Jorge <jorgesifontes.cl@gmail.com>
2020-11-01 22:50:05 -0300

    Building & testing request method

--- prueba_f_intro_ruby.rb
+++ prueba_f_intro_ruby.rb
@@ -1,12 +1,37 @@
 require "uri"
 require "net/http"
+require 'json'
 
-url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2020-11-1&api_key=wj0mdRdT2AjiAi3yOPBz2kjBgvNtxEP7hW0Nf8rz")
+#Request a web site de NASA
+def request (url_requested, api_key)
+    url_builded = url_requested + "?earth_date=2020-11-1&api_key=" + api_key
+    
+    url = URI(url_builded)
 
-https = Net::HTTP.new(url.host, url.port)
-https.use_ssl = true
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
 
-request = Net::HTTP::Get.new(url)
+    request = Net::HTTP::Get.new(url)
 
-response = https.request(request)
-puts response.read_body
+    response = https.request(request)
+
+    resultado = JSON.parse(response.read_body)
+end
+
+# Metodo para construccion de pagina web para visualizacion
+def buid_web_pag (rober_images)
+
+
+end
+
+def photos_count ()
+
+end
+
+
+
+url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
+key = "wj0mdRdT2AjiAi3yOPBz2kjBgvNtxEP7hW0Nf8rz"
+
+#request(url, key)
+
